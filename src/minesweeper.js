6bf8d3754b458a6872beef3a019189ab96ffb147
John Novak <john.novak32@gmail.com>
2017-10-23 19:00:19 -0500

    First commit

---
+++ src/minesweeper.js
@@ -0,0 +1,12 @@
+let guessLine = '1 |   |  ';
+let bombLine = '  | B |  '
+let blankLine = '  |   |  ';
+console.log('This is what an empty board would look like:');
+console.log(blankLine);
+console.log(blankLine);
+console.log(blankLine);
+console.log();
+console.log("This is what a board with a guess and a bomb on it would look like:");
+console.log(guessLine);
+console.log(bombLine);
+console.log(blankLine);
