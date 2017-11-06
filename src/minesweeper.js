acbf1a6b6d1d2df1574295ee5fbbd8ee4915d738
John Novak <john.novak32@gmail.com>
2017-11-05 23:12:51 -0600

    Dynamically generate game boards

--- src/minesweeper.js
+++ src/minesweeper.js
@@ -1,18 +1,46 @@
+const generatePlayerBoard = (numberOfRows, numberOfColumns) => {
+    let board = [];
+    for (let i = 0; i < numberOfRows; i++) {
+        let row = [];
+        for (let j = 0; j < numberOfColumns; j++) {
+            row.push(' ');
+        }
+        board.push(row);
+    }
+    return board;
+}
+
+const generateBombBoard = (numberOfRows, numberOfColumns, numberOfBombs) => {
+    let board = [];
+    for(let i = 0; i < numberOfRows; i++) {
+        let row = [];
+        for (let j = 0; j < numberOfColumns; j++) {
+            row.push(null);
+        }
+        board.push(row);
+    }
+    
+    let numberOfBombsPlaced = 0;
+    
+    while (numberOfBombsPlaced < numberOfBombs) {
+        let randomRowIndex = Math.floor(Math.random() * numberOfRows);
+        let randomColumnIndex = Math.floor(Math.random() * numberOfColumns);
+        // still can potential add bombs over existing bombs
+        board[randomRowIndex][randomColumnIndex] = 'B';
+        numberOfBombsPlaced++;
+    }
+    return board;
+}
+
 const printBoard = (board) => {
-    console.log('Current Board: ');
-    console.log(board[0].join('|'));
-    console.log(board[1].join('|'));
-    console.log(board[2].join('|'));
+    console.log(board.map(row => row.join(' | ')).join('\n'));
 }
 
 
-let board = [ [' ', ' ', ' '], 
-             [' ', ' ', ' '], 
-             [' ', ' ', ' '] ];
+let playerBoard = generatePlayerBoard(3,4);
+let bombBoard = generateBombBoard(3,4,5);
 
-printBoard(board);
-
-board[0][1] = '1';
-board[2][2] = 'B';
-
-printBoard(board);
+console.log('Player Board: ');
+printBoard(playerBoard);
+console.log('Bomb Board: ');
+printBoard(bombBoard);
