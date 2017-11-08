31efe89687805d9263043c3b00c1f8e0e2becb2e
johnmnovak <31937632+johnmnovak@users.noreply.github.com>
2017-11-08 15:16:19 -0600

    proj1 started 11/8

---
+++ webapis/project1/js/game-logic.js
@@ -0,0 +1,20 @@
+// All code should be written in this file.
+let playerOneMoveOneType;
+let playerOneMoveTwoType;
+let playerOneMoveThreeType;
+
+let playerOneMoveOneValue;
+let playerOneMoveTwoValue;
+let playerOneMoveThreeValue;
+
+let playerTwoMoveOneType;
+let playerTwoMoveTwoType;
+let playerTwoMoveThreeType;
+
+let playerTwoMoveOneValue;
+let playerTwoMoveTwoValue;
+let playerTwoMoveThreeValue;
+
+function setPlayerMoves(player, moveOneType, moveOneValue, moveTwoType, moveTwoValue, moveThreeType, moveThreeValue) {
+    
+}
