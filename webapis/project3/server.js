9105a121cb8aa5f6d4b04b624cc64f02fb1f1684
John Novak <john.novak32@gmail.com>
2017-11-09 17:57:27 -0600

    scoop - all complete except bonus

--- webapis/project3/server.js
+++ webapis/project3/server.js
@@ -2,7 +2,9 @@
 let database = {
   users: {},
   articles: {},
-  nextArticleId: 1
+  nextArticleId: 1,
+  comments: {},
+  nextCommentId: 1,
 };
 
 const routes = {
@@ -26,7 +28,20 @@ const routes = {
   },
   '/articles/:id/downvote': {
     'PUT': downvoteArticle
-  }
+  },
+  '/comments': {
+    'POST': createComment
+  },
+  '/comments/:id': {
+    'PUT': updateComment,
+    'DELETE': deleteComment
+  },
+  '/comments/:id/upvote': {
+    'PUT': upvoteComment
+  },
+  '/comments/:id/downvote': {
+    'PUT': downvoteComment
+  },
 };
 
 function getUser(url, request) {
@@ -238,6 +253,104 @@ function downvote(item, username) {
     item.downvotedBy.push(username);
   }
   return item;
+}
+
+function createComment(url, request) {
+    const requestComment = request.body && request.body.comment;
+    const response = {};
+    
+    if (requestComment && requestComment.body && database.articles[requestComment.articleId] && requestComment.username && database.users[requestComment.username]) {
+        const comment = {
+            id: database.nextCommentId++,
+            body: requestComment.body,
+            username: requestComment.username,
+            articleId: requestComment.articleId,
+            upvotedBy: [],
+            downvotedBy: [],
+        };
+        
+        database.comments[comment.id] = comment;
+        database.articles[comment.articleId].commentIds.push(comment.id);
+        database.users[comment.username].commentIds.push(comment.id);
+        
+        response.body = {comment: comment};
+        response.status = 201;
+    }
+    else {
+        response.status = 400;
+    }
+    return response;
+}
+
+function updateComment(url, request) {
+    const id = Number(url.split('/').filter(segment => segment)[1]);
+    const savedComment = database.comments[id];
+    const requestComment = request.body && request.body.comment;
+    const response = {};
+    
+    if(!id || !requestComment || requestComment.body.length <= 0) response.status = 400;
+    else if(!savedComment) response.status = 404;
+    else {
+        savedComment.body = requestComment.body;
+        response.body = {comment: savedComment};
+        response.status = 200;
+        
+    }
+    return response;
+}
+
+function deleteComment(url, request) {
+    const id = Number(url.split('/').filter(segment => segment)[1]);
+    const savedComment = database.comments[id];
+    const response = {};
+    
+    if (savedComment && id){
+        database.comments[id] = null;
+        
+        const userCommentIds = database.users[savedComment.username].commentIds;
+        userCommentIds.splice(userCommentIds.indexOf(id), 1);
+        const articleCommentIds = database.articles[savedComment.articleId].commentIds;
+        articleCommentIds.splice(articleCommentIds.indexOf(id), 1);
+        response.status = 204;
+        
+    }
+    else response.status = 404;
+    
+    return response;
+}
+
+function upvoteComment(url, request) {
+    const id = Number(url.split('/').filter(segment => segment)[1]);
+    const username = request.body && request.body.username;
+    let savedComment = database.comments[id];
+    const response = {};
+    
+    if (savedComment && database.users[username]) {
+        savedComment = upvote(savedComment, username);
+        
+        response.body = {comment: savedComment};
+        response.status = 200;
+    }
+    else response.status = 400;
+    
+    return response;
+}
+
+function downvoteComment(url, request) {
+    const id = Number(url.split('/').filter(segment => segment)[1]);
+    const username = request.body && request.body.username;
+    let savedComment = database.comments[id];
+    const response = {};
+    
+    if (savedComment && database.users[username]) {
+        savedComment = downvote(savedComment, username);
+        
+        response.body = {comment: savedComment};
+        response.status = 200;
+    }
+    else response.status = 400;
+    
+    return response;
 }
 
 // Write all code above this line.
